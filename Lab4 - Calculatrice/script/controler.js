a6ebffbb83858b450796038451e28264be222b94
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-02-05 11:15:18 -0500

    commit du début du jQuery

--- Lab4 - Calculatrice/script/controler.js
+++ Lab4 - Calculatrice/script/controler.js
@@ -2,5 +2,15 @@
  * Created by Daniel on 2/4/2016.
  */
 var calcu = new calculator();
+var onScreen = "0";
 
+window.onload = function(){
 
+    var buttons = $('.button');
+    buttons.forEach(function(){
+
+    })
+
+}
+
+$('#screen').text(onScreen);
