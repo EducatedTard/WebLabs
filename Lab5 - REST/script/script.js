a5305bbf57cab99cde383f3f7c6bc68577ce6b7a
Nathaniel Simard <n.s222att@hotmail.com>
2016-02-13 17:07:35 -0500

    ça marche comme le big big

--- Lab5 - REST/script/script.js
+++ Lab5 - REST/script/script.js
@@ -1,30 +1,54 @@
 var tasks = $("<div>", {class: "task"});
 
+
+function updateDisplay(data) {
+    var newline = document.createElement('tr');
+    var newElementId = document.createElement('td');
+    var newElementTask = document.createElement('td');
+    newElementId.innerHTML = data.id;
+    newElementTask.innerHTML = data.task;
+    newline.appendChild(newElementId);
+    newline.appendChild(newElementTask);
+    document.getElementById('all-notes').appendChild(newline);
+}
+
+function updateTasks() {
+    $.ajax({
+            url: 'http://localhost:5000/tasks',
+            contentType: 'application/json; charset=UTF-8',
+            type: 'GET'
+        })
+        .done(function (data) {
+            var tasks = data.tasks;
+            document.getElementById('all-notes').innerHTML = "";
+            for (var i = 0; i < tasks.length; i++) {
+                updateDisplay(tasks[i]);
+            }
+        })
+        .fail(function (jqXHR, textStatus) {
+
+        });
+}
+
+/*
+ * Add a painful task that will never be done !
+ */
 $('#plus-button').click(function () {
-
-
     var task = {
         "task": $('#input-box').val()
     }
 
     $.ajax({
-        url: 'http://localhost:5000/tasks',
-        contentType: 'application/json; charset=UTF-8',
-        type: 'POST',
-        data: JSON.stringify(task),
-        success: function (data) {
-            $('#input-box').val('');
-        }
-    });
+            url: 'http://localhost:5000/tasks',
+            contentType: 'application/json; charset=UTF-8',
+            type: 'POST',
+            data: JSON.stringify(task)
+        })
+        .done(function (data) {
+        })
+        .fail(function (jqXHR, textStatus) {
 
-
-    $.ajax({
-        url: 'http://localhost:5000/tasks',
-        contentType: 'application/json; charset=UTF-8',
-        type: 'GET',
-        success: function (data) {
-            tasks.html(data);
-            $("#tasks").append(tasks);
-        }
-    })
+        });
+    updateTasks();
 });
+updateTasks();
