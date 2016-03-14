89267fa0d0988b185a8719ef6476042f7de2a481
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-03-13 21:20:33 -0400

    avancement lab 7

--- Lab7- Meteo/script.js
+++ Lab7- Meteo/script.js
@@ -1,34 +1,83 @@
-// Exemple
-var Forecast7DaysModel = Backbone.Model.extend({
-    url: 'http://api.wunderground.com/api/e78755f0aada0cf3/forecast7day/geolookup/q/autoip.json',
+var ForecastModel = Backbone.Model.extend({
+    default:{
+        id: 0
+    },
     parse: function (data) {
-        var location = data.location;
-        var forecastday = data.forecast.simpleforecast.forecastday;
         return {
-            city: location.city,
-            days: forecastday,
-            sunday: [forecastday[0].date.weekday, forecastday[0].icon, forecastday[0].high.celsius, forecastday[0].low.celsius]
+            weekday: data.date.weekday,
+            condition: data.conditions,
+            icon_url: data.icon_url,
+            high: data.high.celsius,
+            low: data.low.celsius
         }
     }
 });
 
 
-var ForecastModel = Backbone.Model.extend({
-    url: 'http://api.wunderground.com/api/e78755f0aada0cf3/forecast7day/geolookup/q/autoip.json',
-    dayId: 0,
-    parse: function (data) {
-        var location = data.location;
-        var day = data.forecast.simpleforecast.forecastday[this.dayId]
-        return {
-            city: location.city,
-            weekday: day.date.weekday,
-            condition: day.conditions,
-            icon_url: day.icon_url,
-            high: day.high.celsius,
-            low: day.low.celsius
-        }
+var cityModel = Backbone.Model.extend({
+    default: {
+        city: "undefined"
+    },
+
+    render: function() {
+        console.log(this.city);
     }
-})
+});
+
+var ForecastCollection = Backbone.Collection.extend({
+    url: 'http://api.wunderground.com/api/e78755f0aada0cf3/forecast7day/geolookup/q/autoip.json',
+    model: ForecastModel,
+    parse: function(data){
+        return data.forecast.simpleforecast.forecastday;
+    }
+});
+
+
+
+var ForecastView = Backbone.View.extend({
+    initialize: function () {
+        _.bindAll(this, 'render');
+        this.collection = new ForecastCollection();
+
+
+        this.listenTo(this.collection, 'sync', this.render);
+        this.collection.fetch();
+    },
+    render: function () {
+        console.log(this.collection.toJSON())
+        var template = _.template($('#forecast-template').html());
+        var compiledTemplate = template({collection: this.collection.models});
+        this.$el.html(compiledTemplate);
+    }
+});
+
+var forecast = new ForecastView();
+
+
+
+
+
+/*
+var Song = Backbone.Model.extend({
+    defaults: {
+        name: "Not specified",
+        artist: "Not specified"
+    },
+    initialize: function(){
+        console.log("Music is the answer");
+    }
+});
+
+var Album = Backbone.Collection.extend({
+    model: Song
+});
+
+var song1 = new Song({ name: "How Bizarre", artist: "OMC" });
+var song2 = new Song({ name: "Sexual Healing", artist: "Marvin Gaye" });
+var song3 = new Song({ name: "Talk It Over In Bed", artist: "OMC" });
+
+var myAlbum = new Album([ song1, song2, song3]);
+console.log( myAlbum.models ); // [song1, song2, song3]
 
 var ForecastView = Backbone.View.extend({
     initialize: function () {
@@ -42,7 +91,6 @@ var ForecastView = Backbone.View.extend({
 });
 
 var forecast = new ForecastView();
-
 
 /*
  var ForecastCollection = Backbone.Collection.extend({
