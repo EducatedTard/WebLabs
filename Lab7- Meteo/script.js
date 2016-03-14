03811ca2893c8ce2c76fe19f1e76323559aafe58
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-03-13 22:23:17 -0400

    FINI

--- Lab7- Meteo/script.js
+++ Lab7- Meteo/script.js
@@ -13,26 +13,16 @@ var ForecastModel = Backbone.Model.extend({
     }
 });
 
-
-var cityModel = Backbone.Model.extend({
-    default: {
-        city: "undefined"
-    },
-
-    render: function() {
-        console.log(this.city);
-    }
-});
+var city = "";
 
 var ForecastCollection = Backbone.Collection.extend({
     url: 'http://api.wunderground.com/api/e78755f0aada0cf3/forecast7day/geolookup/q/autoip.json',
     model: ForecastModel,
     parse: function(data){
+        city = data.location.city;
         return data.forecast.simpleforecast.forecastday;
     }
 });
-
-
 
 var ForecastView = Backbone.View.extend({
     template: _.template($('#forecast-template').html()),
@@ -40,71 +30,13 @@ var ForecastView = Backbone.View.extend({
     initialize: function () {
         _.bindAll(this, 'render');
         this.collection = new ForecastCollection();
-
-
         this.listenTo(this.collection, 'sync', this.render);
         this.collection.fetch();
     },
     render: function () {
-        console.log(this.collection.toJSON());
         this.$el.html(this.template({collection: this.collection.toJSON()}));
+        $('#city').html(city);
     }
 });
 
 var forecast = new ForecastView();
-
-
-
-
-
-/*
-var Song = Backbone.Model.extend({
-    defaults: {
-        name: "Not specified",
-        artist: "Not specified"
-    },
-    initialize: function(){
-        console.log("Music is the answer");
-    }
-});
-
-var Album = Backbone.Collection.extend({
-    model: Song
-});
-
-var song1 = new Song({ name: "How Bizarre", artist: "OMC" });
-var song2 = new Song({ name: "Sexual Healing", artist: "Marvin Gaye" });
-var song3 = new Song({ name: "Talk It Over In Bed", artist: "OMC" });
-
-var myAlbum = new Album([ song1, song2, song3]);
-console.log( myAlbum.models ); // [song1, song2, song3]
-
-var ForecastView = Backbone.View.extend({
-    initialize: function () {
-        this.model = new ForecastModel();
-        this.listenTo(this.model, 'sync', this.render);
-        this.model.fetch();
-    },
-    render: function () {
-        console.log(this.model.toJSON());
-    }
-});
-
-var forecast = new ForecastView();
-
-/*
- var ForecastCollection = Backbone.Collection.extend({
- model: ForecastModel,
- url: 'http://api.wunderground.com/api/e78755f0aada0cf3/forecast7day/geolookup/q/autoip.json'
- });
-
-
- var forecasts = new ForecastCollection();
- */
-
-/*
- var forecasts = new ForecastCollection();
- forecasts.fetch();
- forecasts.bind('reset', function () {
- console.log(forecasts);
- });*/
