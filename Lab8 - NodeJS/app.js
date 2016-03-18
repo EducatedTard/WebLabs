d339839b9a23e6feb645a64132573d7dd8063c3a
nasim80 <n.s222att@hotmail.com>
2016-03-18 13:59:35 -0400

    avec un beau gros module :D :D

--- Lab8 - NodeJS/app.js
+++ Lab8 - NodeJS/app.js
@@ -5,57 +5,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var tasks = [];
-var id = 0;
-
-app.get('/tasks', function (req, res) {
-    res.send(tasks);
-});
-
-app.post('/tasks', function (req, res) {
-    var description = req.body.task;
-    var task = {"id": id, "task": description}
-    tasks.push(task);
-    res.send(task);
-    id = id + 1;
-});
-
-app.get('/tasks/:id', function (req, res) {
-    var requestedTask;
-    tasks.forEach(function (task) {
-        if (task.id == req.params.id) {
-            requestedTask = task;
-        }
-    })
-    res.send(requestedTask);
-
-});
-
-app.put('/tasks/:id', function (req, res) {
-    var description = req.body.task;
-    var taskResponse;
-
-    tasks.forEach(function (task) {
-        if (task.id == req.params.id) {
-            task.task = description;
-            taskResponse = task;
-        }
-    })
-
-    res.send(taskResponse);
-
-});
-
-app.delete('/tasks/:id', function (req, res) {
-    var deletedTask;
-    for (var i = 0; i < tasks.length; i++) {
-        if (tasks[i].id == req.params.id) {
-            deletedTask = tasks[i];
-            tasks.splice(i, 1);
-        }
-    }
-    res.send(deletedTask);
-});
+const tasksModule = require('./tasks.js');
+tasksModule.getTasks(app);
+tasksModule.getTaskId(app);
+tasksModule.deleteTask(app);
+tasksModule.postTask(app);
+tasksModule.putTask(app);
 
 
 app.listen(3000, function () {
---
+++ Lab8 - NodeJS/tasks.js
@@ -0,0 +1,60 @@
+var tasks = [];
+var id = 0;
+
+module.exports = {
+    getTasks: function(app){
+        app.get('/tasks', function (req, res) {
+            res.send(tasks);
+        });
+    },
+    postTask: function(app){
+
+        app.post('/tasks', function (req, res) {
+            var description = req.body.task;
+            var task = {"id": id, "task": description}
+            tasks.push(task);
+            res.send(task);
+            id = id + 1;
+        });
+    },
+    getTaskId: function(app){
+        app.get('/tasks/:id', function (req, res) {
+            var requestedTask;
+            tasks.forEach(function (task) {
+                if (task.id == req.params.id) {
+                    requestedTask = task;
+                }
+            });
+            res.send(requestedTask);
+
+        });
+    },
+    putTask: function(app){
+        app.put('/tasks/:id', function (req, res) {
+            var description = req.body.task;
+            var taskResponse;
+
+            tasks.forEach(function (task) {
+                if (task.id == req.params.id) {
+                    task.task = description;
+                    taskResponse = task;
+                }
+            });
+
+            res.send(taskResponse);
+
+        });
+    },
+    deleteTask: function(app){
+        app.delete('/tasks/:id', function (req, res) {
+            var deletedTask;
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i].id == req.params.id) {
+                    deletedTask = tasks[i];
+                    tasks.splice(i, 1);
+                }
+            }
+            res.send(deletedTask);
+        });
+    }
+};
